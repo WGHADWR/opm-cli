71f7a36fb022c002c07cb6074d3b2ac69fda4c21
unknown <wghadwr@gmail.com>
2020-02-06 17:18:57 +0800

    NONE

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,6 +1,27 @@
+import { HttpClients } from '@/common/HttpClients';
 
 export class AppConfig {
 
   public static readonly ServerUrl = 'http://localhost:8000';
 
+  private static _token : string;
+  public static get Token() : string {
+    return this._token;
+  }
+  public static set Token(v : string) {
+    this._token = v;
+  }
+
+  public static init() {
+    HttpClients.registRequestInterceptor(() => {
+      return {
+        headers: {
+          'access-token': this.Token,
+        },
+      };
+    });
+  }
+
 }
+
+AppConfig.init();
